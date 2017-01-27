05cbdad0b3f382ccf072e59cd1d9d04c69dbddab
Kelly Hancock <kellyhancock@Kellys-MacBook-Pro.local>
2017-01-26 16:55:09 -0800

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,9 +2,10 @@ console.log("JS file is connected to HTML! Woo!");
 
 var cardsOneToFour = ["queen", "king", "queen", "king"];
 
-if("queen" === "queen" && "king"==="kind"){
+if("queen" === "queen" && "king"==="king"){
 	window.alert("You found a match!");
 }
 else{
 	window.alert("Sorry, try again!");
 }
+document.createElement('div')
